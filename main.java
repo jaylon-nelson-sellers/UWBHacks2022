df9dd9b27595b7ad4ff965a1cdfb137125af7adc
Jaylon Nelson-Sellers <jcns@uw.edu>
2022-05-14 14:46:36 -0700

    Added simple psuedocode. Need to figure out why subtitle library doesn't work

---
+++ main.java
@@ -0,0 +1,21 @@
+/**
+ * vttlibrary
+ * main
+ *  runs code
+ *
+ * class keyword
+ *      parse VVT
+ *      createkeywordlist
+ *      outputkeywords
+ *      setLinks
+ *      getlinks
+ *
+ */
+
+public class main{
+
+    public static void main(String args[])  //static method
+    {
+        System.out.println("Static method");
+    }
+}
